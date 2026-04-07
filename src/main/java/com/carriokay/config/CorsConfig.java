4a305e461c638109dcd7718c6935d5de8844698f
PraveenReddy-06 <2400032662@kluniversity.in>
2026-04-07 07:44:21 +0530

    Changes in url

--- src/main/java/com/carriokay/config/CorsConfig.java
+++ src/main/java/com/carriokay/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://carri-okay-412.netlify.app/")
+                .allowedOrigins("https://carri-okay-412.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/carriokay/config/SecurityConfig.java
+++ src/main/java/com/carriokay/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://carri-okay-412.netlify.app/"));
+        config.setAllowedOrigins(List.of("https://carri-okay-412.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
