4d745c9f24056334c35ec71d935773c4b91fe866
PraveenReddy-06 <2400032662@kluniversity.in>
2026-04-07 08:29:55 +0530

    Removed Test annotation

--- src/test/java/com/carriokay/CarriOkayBackendApplicationTests.java
+++ src/test/java/com/carriokay/CarriOkayBackendApplicationTests.java
@@ -1,9 +1,7 @@
 package com.carriokay;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CarriOkayBackendApplicationTests {
 
 	@Test
