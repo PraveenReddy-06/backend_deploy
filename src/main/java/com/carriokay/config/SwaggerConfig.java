dc7027dbeb10ee823db462f0fbb519965aaf092b
PraveenReddy-06 <2400032662@kluniversity.in>
2026-04-17 19:55:15 +0530

    new swagger config class in .config

---
+++ src/main/java/com/carriokay/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.carriokay.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addServersItem(new Server()
+                .url("https://backenddeploy-production-15ff.up.railway.app")
+                .description("Production Server"));
+    }
+}
