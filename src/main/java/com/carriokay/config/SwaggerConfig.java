dc050663c95cc42491e5914a6f11b36315599048
PraveenReddy-06 <2400032662@kluniversity.in>
2026-04-17 21:54:26 +0530

    added authorize key in swagger

--- src/main/java/com/carriokay/config/SwaggerConfig.java
+++ src/main/java/com/carriokay/config/SwaggerConfig.java
@@ -1,17 +1,31 @@
 package com.carriokay.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
             .addServersItem(new Server()
                 .url("https://backenddeploy-production-15ff.up.railway.app")
-                .description("Production Server"));
+                .description("Production Server"))
+            .info(new Info().title("Carri Okay API").version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+            .components(new Components()
+                .addSecuritySchemes("Bearer Auth", new SecurityScheme()
+                    .name("Bearer Auth")
+                    .type(Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
     }
 }
